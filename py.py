ac6a75a06a322cc61075ac2c8da7fe46a9427384
Bilal2504 <98469209+Bilal2504@users.noreply.github.com>
2024-09-10 13:57:35 +0200

    Update py.py

--- py.py
+++ py.py
@@ -4,7 +4,7 @@ import pandas as pd
 fichier_initial = "20240801_etat_validation_paa_chantiers.csv"
 df = pd.read_csv(fichier_initial)
 
-# Renommage des colonnes
+# Paramétrage du renommage des colonnes
 colonne_extraire = {
     "Ligne Projet ou Chantier": "Type",
     "Projet / sous-projets Code projet ou sous-projet présent dans le référentiel des projets SI": "Projet",
@@ -24,19 +24,10 @@ df_filtre = df_selection[df_selection["Type"] == "Projet"]
 df_trie = df_filtre.sort_values(by=["Domaine", "Sous-domaine"], ascending=[True, True])
 
 # Nettoyage des données dans la colonne "Charges SI (Interne/Externe) validée"
-#(remplace les "," par des "." , les valeurs vide par des "0" et transforme toutes les valeur en valeur numerique)
 df_trie["Charges SI (Interne/Externe) validée"] = df_trie["Charges SI (Interne/Externe) validée"].str.strip()
 df_trie["Charges SI (Interne/Externe) validée"] = df_trie["Charges SI (Interne/Externe) validée"].str.replace(',', '.')
 df_trie["Charges SI (Interne/Externe) validée"] = df_trie["Charges SI (Interne/Externe) validée"].fillna(0)
 df_trie["Charges SI (Interne/Externe) validée"] = pd.to_numeric(df_trie["Charges SI (Interne/Externe) validée"], errors='coerce')
-
-# Calcul de la somme totale des charges
-somme_total = df_trie["Charges SI (Interne/Externe) validée"].sum()
-
-# Ajouter la somme totale en première ligne
-df_trie['Somme total'] = ""
-df_trie.loc[df_trie.index[0], "Somme total"] = somme_total
-
 
 # Fonction pour ajouter des lignes de somme par sous-domaine et domaine
 def ajouter_ligne_somme_par_domaine(df):
@@ -47,7 +38,7 @@ def ajouter_ligne_somme_par_domaine(df):
     for domaine, groupe_domaine in df.groupby("Domaine"):
         # Grouper par Sous-domaine pour traiter chaque sous-domaine dans ce domaine
         for sous_domaine, groupe_sous_domaine in groupe_domaine.groupby("Sous-domaine"):
-            # Calculer la somme des charges pour un sous-domaine
+            # Calculer la somme des charges pour ce sous-domaine
             somme_sous_domaine = groupe_sous_domaine["Charges SI (Interne/Externe) validée"].sum()
             
             # Ajouter le groupe de sous-domaine
@@ -57,15 +48,10 @@ def ajouter_ligne_somme_par_domaine(df):
             ligne_somme_sous_domaine = pd.DataFrame({
                 "Type": [""],
                 "Projet": [""],
-                "Domaine": [""],
-                "Sous-domaine": [sous_domaine],
-                "Charges SI (Interne/Externe) validée": [""],
-                "Somme total": [""],
-                "Total domaine": [""],
-                "Total sous-domaine": [f"{somme_sous_domaine}"],
-                "Poids total par domaine": [""],
-                "Poids total par sous-domaine": [poids_sous_domaine],
-                "Poids du sous-domaine dans le SI": [poids_sous_domaine_si]
+                "Domaine": [domaine],
+                "Sous-domaine": [f"Total {sous_domaine}"],
+                "Charges SI (Interne/Externe) validée": [somme_sous_domaine],
+                "Somme total": [""]
             })
             
             # Ajouter la ligne de somme du sous-domaine
@@ -78,15 +64,10 @@ def ajouter_ligne_somme_par_domaine(df):
         ligne_somme_domaine = pd.DataFrame({
             "Type": [""],
             "Projet": [""],
-            "Domaine": [domaine],
+            "Domaine": [f"Total {domaine}"],
             "Sous-domaine": [""],
-            "Charges SI (Interne/Externe) validée": [""],
-            "Somme total": [""],
-            "Total domaine": [f"{somme_domaine}"],
-            "Total sous-domaine": [""],
-            "Poids total par domaine": [poids_domaine],
-            "Poids total par sous-domaine": [""],
-            "Poids du sous-domaine dans le SI": [""]
+            "Charges SI (Interne/Externe) validée": [somme_domaine],
+            "Somme total": [""]
         })
         
         # Ajouter la ligne de somme du domaine
@@ -99,7 +80,7 @@ def ajouter_ligne_somme_par_domaine(df):
 df_final = ajouter_ligne_somme_par_domaine(df_trie)
 
 # Sauvegarder les résultats dans un nouveau fichier CSV
-fichier_final = "copsi_mission_test_25.csv"
+fichier_final = "copsi_mission_test_22.csv"
 df_final.to_csv(fichier_final, index=False)
 
 # Confirmation de la création du fichier
